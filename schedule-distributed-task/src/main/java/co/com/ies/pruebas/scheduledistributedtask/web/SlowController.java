1e6ff39a55a1b2bc2241466da320808ea7c645ab
andres.montoya <andres.montoya@iesonline.com.co>
2023-07-25 18:22:34 -0500

    feat: agregar metodo que programa la tarea

--- schedule-distributed-task/src/main/java/co/com/ies/pruebas/scheduledistributedtask/web/SlowController.java
+++ schedule-distributed-task/src/main/java/co/com/ies/pruebas/scheduledistributedtask/web/SlowController.java
@@ -1,6 +1,5 @@
 package co.com.ies.pruebas.scheduledistributedtask.web;
 
-
 import co.com.ies.pruebas.scheduledistributedtask.service.SlowService;
 import co.com.ies.pruebas.scheduledistributedtask.persistence.SlowView;
 import co.com.ies.pruebas.scheduledistributedtask.config.MeasureTime;
@@ -10,6 +9,7 @@ import static org.jobrunr.scheduling.JobBuilder.*;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.time.Duration;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,24 +28,24 @@ public class SlowController {
 
     @MeasureTime
     @GetMapping("/vista")
-    public ResponseEntity<SlowView> getVistaLenta(){
+    public ResponseEntity<SlowView> getVistaLenta() {
         return ResponseEntity.of(slowService.getVistaLenta());
     }
 
     @MeasureTime
     @GetMapping("/vista2")
-    public ResponseEntity<String> getVistaLentaContainers(){
+    public ResponseEntity<String> getVistaLentaContainers() {
         slowService.test();
         return ResponseEntity.ok("VistaLentaContainers");
     }
 
     @MeasureTime
     @GetMapping("/vista3")
-    public ResponseEntity<String> getVistaLentaContainersQeue(){
-        //jobScheduler.enqueue(() -> slowService.queryHighContainers());
+    public ResponseEntity<String> getVistaLentaContainersQeue() {
+        // jobScheduler.enqueue(() -> slowService.queryHighContainers());
         String ip = "0";
         try {
-            String hostAddress = InetAddress.getLocalHost().getHostAddress() ;
+            String hostAddress = InetAddress.getLocalHost().getHostAddress();
             String hostName = InetAddress.getLocalHost().getHostName();
             ip = hostAddress;
         } catch (UnknownHostException e) {
@@ -53,8 +53,28 @@ public class SlowController {
         }
 
         jobScheduler.create(aJob()
-    .withName("Generate sales report"  + ip)
-    .withDetails(() -> slowService.queryHighContainers()));
+                .withName("Generate sales report" + ip)
+                .withDetails(() -> slowService.queryHighContainers()));
+        return ResponseEntity.ok("queryHighContainers");
+    }
+
+    @MeasureTime
+    @GetMapping("/vista4")
+    public ResponseEntity<String> getVistaLentaContainersSchedule() {
+        // jobScheduler.enqueue(() -> slowService.queryHighContainers());
+        String ip = "0";
+        try {
+            String hostAddress = InetAddress.getLocalHost().getHostAddress();
+            String hostName = InetAddress.getLocalHost().getHostName();
+            ip = hostAddress;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        jobScheduler.create(aJob()
+                .scheduleIn(Duration.ofSeconds(10))
+                .withName("Generate sales report" + ip)
+                .withDetails(() -> slowService.queryHighContainers()));
         return ResponseEntity.ok("queryHighContainers");
     }
 }
