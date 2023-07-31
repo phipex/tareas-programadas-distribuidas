a51a6d3317f94cb3b7e6fcf4f97efb02903035c2
Andres Felipe (@phipex) Montoya <andres.montoya@iesonline.com.co>
2023-07-31 10:01:47 -0500

    feat: redisson con exceutor y remote service

--- schedule-distributed-task/src/main/java/co/com/ies/pruebas/scheduledistributedtask/config/RedissonSpringDataConfig.java
+++ schedule-distributed-task/src/main/java/co/com/ies/pruebas/scheduledistributedtask/config/RedissonSpringDataConfig.java
@@ -1,11 +1,15 @@
 package co.com.ies.pruebas.scheduledistributedtask.config;
 
 import java.io.IOException;
+import java.util.Collections;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.redisson.config.RedissonNodeConfig;
 import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +17,9 @@ import org.springframework.core.io.Resource;
 
 @Configuration
  public class RedissonSpringDataConfig {
+
+    @Autowired
+    private BeanFactory beanFactory;
 
     @Bean
     public RedissonConnectionFactory redissonConnectionFactory(RedissonClient redisson) {
@@ -22,7 +29,12 @@ import org.springframework.core.io.Resource;
     @Bean(destroyMethod = "shutdown")
     public RedissonClient redisson(@Value("classpath:/redisson.yaml") Resource configFile) throws IOException {
         Config config = Config.fromYAML(configFile.getInputStream());
-        return Redisson.create(config);
+            RedissonNodeConfig nodeConfig = new RedissonNodeConfig(config);
+            nodeConfig.setExecutorServiceWorkers(Collections.singletonMap("myExecutor", 2));
+            System.out.println("---------------------------RedissonSpringDataConfig.redisson()");
+            System.out.println(beanFactory);
+            nodeConfig.setBeanFactory(beanFactory);
+        return Redisson.create(nodeConfig);
     }
 
  }
--- schedule-distributed-task/src/main/java/co/com/ies/pruebas/scheduledistributedtask/web/SlowController.java
+++ schedule-distributed-task/src/main/java/co/com/ies/pruebas/scheduledistributedtask/web/SlowController.java
@@ -5,7 +5,20 @@ import co.com.ies.pruebas.scheduledistributedtask.persistence.Execution;
 import co.com.ies.pruebas.scheduledistributedtask.persistence.ExecutionRepository;
 import co.com.ies.pruebas.scheduledistributedtask.persistence.SlowView;
 import co.com.ies.pruebas.scheduledistributedtask.service.SlowService;
+
+import org.redisson.api.RAtomicLong;
+import org.redisson.api.RExecutorService;
 import org.redisson.api.RedissonClient;
+import org.redisson.api.WorkerOptions;
+import org.redisson.api.annotation.RInject;
+import org.redisson.api.executor.TaskFailureListener;
+import org.redisson.api.executor.TaskFinishedListener;
+import org.redisson.api.executor.TaskStartedListener;
+import org.redisson.api.executor.TaskSuccessListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +26,61 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.time.ZonedDateTime;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class SlowController {
+
+    Logger logger = LoggerFactory.getLogger(SlowController.class);
 
     private final SlowService slowService;
 
     private final ExecutionRepository executionRepository;
 
     private final RedissonClient client;
+
+    @Autowired
+    private BeanFactory beanFactory;
+
+    WorkerOptions options = WorkerOptions.defaults()
+
+            // Defines workers amount used to execute tasks.
+            // Default is 1.
+            .workers(2)
+
+    //.beanFactory(beanFactory)
+
+            // add listener which is invoked on task successed event
+            .addListener(new TaskSuccessListener() {
+
+                @Override
+                public <T> void onSucceeded(String taskId, T result) {
+                    logger.info("onSucceeded taskId {}", taskId);
+                    logger.info("onSucceeded tresult {}", result);
+                }
+            })
+
+            // add listener which is invoked on task failed event
+            .addListener(new TaskFailureListener() {
+                public void onFailed(String taskId, Throwable exception) {
+                    logger.info("TaskFailureListener taskId {}", taskId);
+                    logger.info("TaskFailureListener exception {}", exception);
+                }
+            })
+
+            // add listener which is invoked on task started event
+            .addListener(new TaskStartedListener() {
+                public void onStarted(String taskId) {
+                    logger.info("TaskFailureListener taskId {}", taskId);
+                }
+            })
+
+            // add listener which is invoked on task finished event
+            .addListener(new TaskFinishedListener() {
+                public void onFinished(String taskId) {
+                    logger.info("TaskFailureListener taskId {}", taskId);
+                }
+            });
 
     public SlowController(SlowService slowService, ExecutionRepository executionRepository, RedissonClient client) {
         this.slowService = slowService;
@@ -59,7 +118,6 @@ public class SlowController {
         execution.setCalled(ZonedDateTime.now());
         Execution saved = executionRepository.save(execution);
 
-
         return ResponseEntity.ok("queryHighContainers");
     }
 
@@ -76,23 +134,48 @@ public class SlowController {
             e.printStackTrace();
         }
 
-
         return ResponseEntity.ok("queryHighContainers");
     }
 
     @MeasureTime
     @GetMapping("/vista5")
-    public ResponseEntity<String> remote(){
-        callTask();
-callTask();
-callTask();
-callTask();
-callTask();
-callTask();
+    public ResponseEntity<String> remote() {
+        RExecutorService executor = client.getExecutorService("myExecutor");
+        executor.registerWorkers(options);
+        callTask(executor);
+        callTask(executor);
+        callTask(executor);
+        callTask(executor);
+        callTask(executor);
+        callTask(executor);
         return ResponseEntity.ok("remote");
     }
 
-    private void callTask() {
+    private void callTask(RExecutorService executor) {
+
+
+
+        String ip = "0";
+        try {
+            String hostAddress = InetAddress.getLocalHost().getHostAddress();
+            String hostName = InetAddress.getLocalHost().getHostName();
+            ip = hostAddress;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        Execution execution = new Execution();
+        execution.setLaunch(ip);
+        execution.setCalled(ZonedDateTime.now());
+        Execution saved = executionRepository.save(execution);
+
+        //service.queryHighContainers(saved.getId());
+        executor.submit(new RunnableTask(saved.getId()));
+    }
+
+    private void callTask0() {
+
+
+
         SlowService service = client.getRemoteService().get(SlowService.class);
 
         String ip = "0";
@@ -109,5 +192,24 @@ callTask();
         Execution saved = executionRepository.save(execution);
 
         service.queryHighContainers(saved.getId());
+    }
+
+    public static class RunnableTask implements Runnable {
+
+    @RInject
+    private RedissonClient redissonClient;
+
+        private long param;
+
+        public RunnableTask(long param) {
+            this.param = param;
+        }
+
+        @Override
+        public void run() {
+            SlowService service = redissonClient.getRemoteService().get(SlowService.class);
+            service.queryHighContainers(param);
+        }
+
     }
 }
